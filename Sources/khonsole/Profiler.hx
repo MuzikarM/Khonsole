ca227263ab0f28a4576bb29f5dc395395b89dfa4
Martin Muzikář <muzikarpc@gmail.com>
2017-07-15 18:01:06 +0200

    Profiler is now scrollable

--- Sources/khonsole/Profiler.hx
+++ Sources/khonsole/Profiler.hx
@@ -5,6 +5,9 @@ class Profiler extends Window{
 
 	public var profiles(default, null):Map<String, Profile>;
 	var columnWidth:Float;
+	var index:Int;
+	var maxIndex:Int;
+	var totalProfiles:Int;
 
 	public function new(x:Int, y:Int, w:Int, h:Int){
 		profiles = new Map<String, Profile>();
@@ -13,6 +16,10 @@ class Profiler extends Window{
 		showing = false;
 		columnWidth = w / 4;
 		this.onResize = _resize;
+		this.onScroll = _scroll;
+		index = 0;
+		totalProfiles = 0;
+		maxIndex = Std.int((bounds.h - Khonsole.fontSize)/ Khonsole.fontSize);
 		addCloseButton();
 		#if (!js && !flash)
 		addButton(new Button(0.95, 0, "Save", _save));
@@ -20,7 +27,13 @@ class Profiler extends Window{
 	}
 
 	function _scroll(i:Int){
-		
+		if (totalProfiles < maxIndex)
+			return;
+		index += i;
+		if (index < 0)
+			index = 0;
+		if (index > totalProfiles - maxIndex)
+			index= totalProfiles - maxIndex;
 	}
 
 	function _save(_){
@@ -51,6 +64,7 @@ class Profiler extends Window{
 				lastTime: 0,
 				lastStartTime: haxe.Timer.stamp()
 			});
+			totalProfiles++;
 		} else {
 			var prof = profiles.get(name);
 			profiles.set(name, {
@@ -114,13 +128,22 @@ class Profiler extends Window{
 		if (!showing)
 			return;
 		prepareWindow(g);
-		g.opacity = 1;
-		g.color = 0xffffffff;
+		g.color = 0xff000000;
+		g.opacity = Khonsole.opacity;		
 		drawColumns(g, 1, ["Name", "Last time", "Calls", "Avg."]);
 		var i = 2;
+		g.scissor(bounds.x, bounds.y + i * g.fontSize, bounds.w, bounds.h - i * g.fontSize);
+		g.translate(0, -index * g.fontSize);
 		for (profile in profiles){
 			drawProfile(g, i, profile);
 			i++;
+		}
+		g.disableScissor();
+		g.translate(0, index * g.fontSize);		
+		if (totalProfiles > maxIndex){
+			g.color = 0xffffffff;
+			g.fillRect(bounds.x + bounds.w - 4, g.fontSize * 2 + bounds.h * (index / totalProfiles), 2, bounds.h * (maxIndex / totalProfiles) - g.fontSize * 2);
+			g.color = 0xff000000;
 		}
 	}
 
